c898fbd036916c8aad919b552613a4ecf749d860
Artemiks <fil.ar.alex@gmail.com>
2021-09-26 19:38:48 +0300

    mb bb

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,4 +1,22 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+
+	"github.com/Artemchikus/internal/handler"
+	"github.com/go-chi/chi"
+)
+
+func main() {	
+
+	h := handler.NewHandler()	
+
+	r := chi.NewRouter()
+
+	r.Get("/hello", h.Hello)
+
+	err := http.ListenAndServe(":8080", r)
+	log.Fatal(err)
+
 }
