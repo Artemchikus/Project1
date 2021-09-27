ec7348a7ce34d25f38b410db94d0163abe1b2400
Artemiks <fil.ar.alex@gmail.com>
2021-09-27 13:45:24 +0300

    port & host

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -4,11 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Artemchikus/internal/config"
 	"github.com/Artemchikus/internal/handler"
+
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 func main() {	
+
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	h := handler.NewHandler()	
 
@@ -17,7 +26,7 @@ func main() {
 	r.Get("/hello", h.Hello)
 
 	log.Print("Satrting server:")
-	err := http.ListenAndServe(":8080", r)
+	err = http.ListenAndServe(":8080", r)
 	log.Fatal(err)
 
 	log.Print("Shutting server down:")
